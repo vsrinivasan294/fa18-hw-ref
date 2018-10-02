3555950a038a81a912c55363a1af97fbe0678ce2
Vishnu Srinivasan <vishnunaga_srinivasan@gmail.com>
2018-10-02 10:15:09 -0500

    Half working commit

--- hw3.py
+++ hw3.py
@@ -1,3 +1,7 @@
+# All code written assuming there are no checks for sideeffeects beacause it was not mentioned otherwise anywhere
+
+
+
 """
 matrix_multiply
 
@@ -6,9 +10,10 @@ Return None if the matrix product does not exist.
 
 As with math, assume that indices are in [row][column] format, so each inner list is a row.
 """
-def matrix_multiply(arr0, arr1):
-	pass
 
+def matrix_multiply(arr0, arr1):
+   
+    
 """
 nth_largest_element
 
@@ -16,7 +21,15 @@ Given an input list `arr`, and index `n`, return the nth largest element.
 Avoid using built-in sorting methods.
 """
 def nth_largest_element(arr, n):
-	pass
+    
+    check =[]
+    for x in range (n):
+        check.append(max(arr))
+        max_val = arr.index(max(arr))
+        arr.pop(max_val)
+        
+    return check[n-1]
+
 
 """
 reverse_block
@@ -32,8 +45,26 @@ Example:
 
 """
 def reverse_block(arr, n):
-	pass
-
+    if (len(arr) == 0):
+        return None
+    
+    i = 0
+    
+    while(i<len(arr)): 
+      
+        val_one = i  
+        val_two = min(i + n - 1, len(arr) - 1)
+        check = (val_two - val_one)/2
+        while (check > 0): 
+           
+            arr[val_one], arr[val_two] = arr[val_two], arr[val_one] 
+            val_one = val_one + 1 
+            val_two = val_two - 1
+            check = check - 1
+            
+        i+= n      
+    return arr
+    
 """
 subset_sum
 
@@ -52,9 +83,21 @@ Example 2:
 	Return:
 		False
 """
-def subset_sum(arr, target):
-	pass
+def checker(arr ,arr_length, target) : 
+    
+    if (target == 0) : 
+        return True
+    elif (arr_length == 0 and target != 0): 
+        return False
+   
+    elif (arr[arr_length - 1] > target) : 
+        return checker(arr, arr_length - 1, target) 
+   
+    else :
+        return checker(arr, arr_length-1, target) or checker(arr, arr_length-1, target - arr[arr_length-1]) 
 
+def subset_sum(arr, target):
+    return checker(arr, len(set), target)
 """
 spiral_matrix
 
@@ -71,5 +114,62 @@ Example:
 		[a,b,c,d,e, j,o,t,y, x,w,v,u, p,k,f, g,h,i, n,s, r,q, l, m]
 """
 def spiral_matrix(arr):
-	pass
+   
+
+    if (arr is None):
+        return None
+    if (len(arr) == 0):
+        return None
+    
+    #Stores the output list
+    output = []
+    
+    #Counts the number of Rows completed
+    row_count = 0
+    
+    #Counts the number of Columns completed
+    column_count = 0
+    
+    #The number of Rows
+    last_row = len(arr) - 1
+    
+    #The number of columns
+    last_column = len(arr[0]) - 1
+
+    while True:
+        for x in range(column_count, last_column + 1):
+            output.append((arr[row_count][x]))
+        
+        # If the row count is larger than the maximum number of rows, it is a L, so return whatever I have already
+        row_count += 1
+        if (row_count > last_row):
+            return output
+
+        for x in range(row_count, last_row + 1):
+            output.append((arr[x][last_column]))
+       
+        #If the column count is greater than the number of columns it is a L, so return whatever I have already
+        last_column -= 1
+        if  (column_count > last_column):
+            return output
+
+        # This works when it is needed to check the values backwards when the rotation happens
+        for x in range(column_count, last_column + 1)[::-1]:
+            output.append((arr[last_row][x]))
+        
+        # If the row count is larger than the maximum number of rows, it is a L, so return whatever I have already
+        last_row -= 1
+        if (row_count > last_row):
+            return output
+
+        # This works when it is needed to check the values backwards when the rotation happens
+        for x in range(row_count, last_row + 1)[::-1]:
+            output.append((arr[x][column_count]))
+        
+        #If the column count is greater than the number of columns it is a L, so return whatever I have already    
+        column_count += 1
+        if (column_count > last_column):
+            return output
+        
+
 
